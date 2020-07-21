de50a8e36bb8e4c500759789d58b1a3f66a4b8a1
Praveen <praveensbj8@gmail.com>
2020-07-21 21:11:26 +0530

    Updates

---
+++ precache-manifest.1e463d379c0a58043d1d9c68215ffe89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e303743429ebfe75b9715ca5471f26f0",
+    "url": "/HR-employee-app/index.html"
+  },
+  {
+    "revision": "c5ef68b1c005d4e6c6a4",
+    "url": "/HR-employee-app/static/css/main.b3816790.chunk.css"
+  },
+  {
+    "revision": "e8c887e8946f8642ab2d",
+    "url": "/HR-employee-app/static/js/2.22eddb03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HR-employee-app/static/js/2.22eddb03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5ef68b1c005d4e6c6a4",
+    "url": "/HR-employee-app/static/js/main.38c9a8d2.chunk.js"
+  },
+  {
+    "revision": "ef34fb07676bb810c3a0",
+    "url": "/HR-employee-app/static/js/runtime-main.1ccfc025.js"
+  }
+]);
